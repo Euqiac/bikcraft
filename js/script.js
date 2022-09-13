3896f469eac64514265deb3758353c88d918bdbd
Caíque Alves <caiquealves1312@hotmail.com>
2022-09-12 23:48:27 -0300

    Foram adicionadas algumas funções com js para preenchimento automatico dos campos de orçamento, indicação da página atual no menu superior e ocultação e mostragem das perguntas frequentes

---
+++ js/script.js
@@ -0,0 +1,52 @@
+// Ativar links do menu
+const links = document.querySelectorAll(".header-menu a");
+
+function ativarLink(link){
+    const url = window.location.href;
+    const href = link.href;
+
+    if (url.includes(href)){
+        link.classList.add("ativo");
+    }
+    
+}
+
+links.forEach(ativarLink);
+
+
+// Ativar itens do orçamento
+const parametros = new URLSearchParams(location.search);
+console.log(parametros);
+
+function ativarProduto(parametro){
+    const elemento = document.getElementById(parametro);
+    if (elemento){
+        elemento.checked = true;
+    }
+}
+parametros.forEach(ativarProduto);
+
+
+//Perguntas Frequentes
+const perguntas = document.querySelectorAll(".perguntas button");
+
+function ativarPergunta(event) {
+    const pergunta = event.currentTarget;
+    const control = pergunta.getAttribute('aria-controls');
+    const resposta = document.getElementById(control);
+
+    resposta.classList.toggle('ativa');
+    const ativa  =  resposta.classList.contains('ativa')
+    pergunta.setAttribute("aria-expanded", ativa)
+
+
+
+
+    console.log(control)
+}
+
+function eventosPerguntas(pergunta){
+    pergunta.addEventListener("click", ativarPergunta)
+}
+
+perguntas.forEach(eventosPerguntas);
